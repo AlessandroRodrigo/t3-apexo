14b758463642887bd23570c788a1e5e6b6889d85
Alessandro Rodrigo <97448512+AlessandroRodrigo@users.noreply.github.com>
2024-05-18 14:19:54 -0300

    refactor: Add user and chat table schemas with relations

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
 
 /**
@@ -27,17 +27,46 @@ export const posts = createTable(
   }),
 );
 
-export const chats = createTable(
-  "chat",
+export const users = createTable(
+  "user",
   {
     id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
     name: text("name", { length: 256 }),
+    clerkId: int("clerk_id", { mode: "number" }),
+    stripeId: text("stripe_id", { length: 256 }),
     createdAt: int("created_at", { mode: "timestamp" })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
     updatedAt: int("updatedAt", { mode: "timestamp" }),
   },
   (example) => ({
-    nameIndex: index("chat_name_idx").on(example.name),
+    nameIndex: index("user_name_idx").on(example.name),
   }),
 );
+
+export const chats = createTable(
+  "chat",
+  {
+    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    userId: int("user_id", { mode: "number" }),
+    threadId: text("thread_id"),
+    createdAt: int("created_at", { mode: "timestamp" })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: int("updatedAt", { mode: "timestamp" }),
+  },
+  (example) => ({
+    nameIndex: index("chat_thread_idx").on(example.threadId),
+  }),
+);
+
+export const userRelations = relations(users, ({ many }) => ({
+  chats: many(chats),
+}));
+
+export const chatRelations = relations(chats, ({ one }) => ({
+  user: one(users, {
+    fields: [chats.userId],
+    references: [users.id],
+  }),
+}));
