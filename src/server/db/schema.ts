d5e9475a6069c7b085debd6582421442d3b88937
Alessandro Rodrigo <97448512+AlessandroRodrigo@users.noreply.github.com>
2024-05-17 23:41:58 -0300

    refactor: Add chats table to the database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -24,5 +24,20 @@ export const posts = createTable(
   },
   (example) => ({
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
+);
+
+export const chats = createTable(
+  "chat",
+  {
+    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    name: text("name", { length: 256 }),
+    createdAt: int("created_at", { mode: "timestamp" })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: int("updatedAt", { mode: "timestamp" }),
+  },
+  (example) => ({
+    nameIndex: index("chat_name_idx").on(example.name),
+  }),
 );
