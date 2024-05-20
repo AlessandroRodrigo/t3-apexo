b4bfed907e57fd647b5efb9c4493af750a3c477e
Alessandro Rodrigo <97448512+AlessandroRodrigo@users.noreply.github.com>
2024-05-19 21:45:36 -0300

    refactor: Update landing page layout and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, Instagram } from "lucide-react";
 import Image from "next/image";
 import { Navigation } from "~/app/_components/landing/nav";
 import { Button } from "~/components/ui/button";
@@ -40,9 +40,9 @@ export default async function Home() {
         </nav>
       </header>
 
-      <section className="relative isolate flex min-h-screen flex-col px-6 pt-14 lg:px-40">
+      <section className="relative isolate flex flex-col px-6 pt-14 lg:px-40">
         <div className="flex gap-4">
-          <div className="mr-auto flex max-w-2xl grow items-center py-32 sm:py-48 lg:py-56">
+          <div className="mr-auto flex max-w-2xl grow py-32 sm:py-48 lg:py-56">
             <div className="text-left">
               <h1 className="text-5xl font-bold tracking-tight text-stone-200 sm:text-7xl">
                 How big would you dream, if you knew you{" "}
@@ -58,20 +58,15 @@ export default async function Home() {
             height={500}
           />
         </div>
-        <div className="flex justify-center">
-          <Button size="lg" className="flex gap-2">
-            <ArrowDown />
-            Discover all solutions
-          </Button>
-        </div>
       </section>
 
       <section className="isolate flex flex-row items-center justify-center gap-16 px-6 py-20 lg:px-40">
-        <h2 className="text-5xl font-bold">
+        <h2 className="bg-gradient-to-l from-secondary from-10% to-stone-200 to-90% bg-clip-text text-5xl font-bold text-transparent">
           What <br /> We Do
         </h2>
+
         <Separator orientation="vertical" className="h-24 w-1" />
-        <p className="max-w-md text-lg text-stone-400">
+        <p className="max-w-md text-lg font-thin text-stone-400">
           Imagine a world where your business is limitless. Where technology and
           creativity come together to unlock new possibilities. Welcome to
           Apexo, your partner in AI-driven innovation.
@@ -79,8 +74,31 @@ export default async function Home() {
       </section>
 
       <section className="isolate flex flex-row items-center justify-center gap-16 px-6 py-20 lg:px-40">
-        <h2 className="text-4xl font-bold">Trusted by:</h2>
+        <h2 className="text-4xl font-bold text-stone-200">Trusted by:</h2>
       </section>
+
+      <footer className="relative isolate flex h-72 flex-col justify-between gap-8 px-6 py-6 md:h-96 md:px-40 md:py-14">
+        <Image src="/apexo-logo.svg" alt="" width={100} height={100} />
+
+        <div className="flex flex-col items-start justify-between gap-4 md:flex-row md:items-center md:gap-0">
+          <div className="flex items-center gap-2">
+            <div className="rounded-full border border-stone-500 p-2">
+              <Image src="/instagram-logo.svg" alt="" width={20} height={20} />
+            </div>
+            <div className="rounded-full border border-stone-500 p-2">
+              <Image src="/whatsapp-logo.svg" alt="" width={20} height={20} />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <span>
+              A product of <strong>Apexo</strong>
+            </span>
+
+            <span>2024 | All rights reserved</span>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
