af62486941384fbe97f909141b90e150a52c9382
Alessandro Rodrigo <97448512+AlessandroRodrigo@users.noreply.github.com>
2024-05-19 22:03:39 -0300

    refactor: Update CallToAction component layout and styles

--- src/app/_components/landing/call-to-action.tsx
+++ src/app/_components/landing/call-to-action.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export function CallToAction() {
   return (
-    <div className="relative isolate flex flex-col px-6 pt-14 lg:px-40">
+    <div className="relative isolate flex flex-col px-6 py-14 lg:px-40">
       <div className="relative isolate overflow-hidden bg-stone-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
         <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
           <h2 className="text-3xl font-bold tracking-tight text-stone-200 sm:text-4xl">
