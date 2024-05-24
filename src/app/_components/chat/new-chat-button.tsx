5a5e2751234a76e8c674e3bfa4dc5f6b3c821178
Alessandro Rodrigo <97448512+AlessandroRodrigo@users.noreply.github.com>
2024-05-24 00:47:02 -0300

    refactor: Add NewChatButton component for creating new chats

---
+++ src/app/_components/chat/new-chat-button.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Pencil, Plus } from "lucide-react";
+import { useRouter } from "next/router";
+import { Button } from "~/components/ui/button";
+
+export function NewChatButton() {
+  const router = useRouter();
+
+  function handleCreateNewChat() {
+    void router.push("/tools/oliwrite");
+  }
+
+  return (
+    <>
+      <Button
+        size="icon"
+        variant="outline"
+        className="mt-0 hidden lg:block"
+        onClick={handleCreateNewChat}
+      >
+        <Plus className="h-5 w-5" />
+      </Button>
+      <Button
+        className="flex items-center gap-2 lg:hidden"
+        variant="outline"
+        onClick={handleCreateNewChat}
+      >
+        <Pencil className="h-5 w-5" />
+        Create new chat
+      </Button>
+    </>
+  );
+}
