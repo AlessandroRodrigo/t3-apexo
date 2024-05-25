2dcf176a481bb8952cb597362fc4187d9b5488e4
Alessandro Rodrigo <97448512+AlessandroRodrigo@users.noreply.github.com>
2024-05-24 23:11:05 -0300

    refactor: Update oliwrite page to use lazy loading for chat messages

--- src/app/tools/oliwrite/[[...chatId]]/page.tsx
+++ src/app/tools/oliwrite/[[...chatId]]/page.tsx
@@ -1,41 +1,42 @@
+import { type Metadata } from "next";
 import { Chat } from "~/app/_components/chat/chat";
 import { ChatHistory } from "~/app/_components/chat/history";
 
-// export const metadata: Metadata = {
-//   title: "Apexo | OliWrite - Elevate your script writing game",
-//   description:
-//     "Oliwrite is a versatile tool offered by Apexo Digital, designed to enhance your digital writing experience.",
-//   icons: [{ rel: "icon", url: "/favicon.ico" }],
-//   abstract: "width=device-width, initial-scale=1",
-//   appleWebApp: true,
-//   applicationName: "Apexo",
-//   category: "Business",
-//   classification: "Business",
-//   keywords: [
-//     "Apexo Digital",
-//     "Oliwrite",
-//     "writing tool",
-//     "digital writing",
-//     "content creation",
-//     "writing enhancement",
-//   ],
-//   openGraph: {
-//     title: "Apexo | OliWrite - Elevate your script writing game",
-//     description:
-//       "Oliwrite is a versatile tool offered by Apexo Digital, designed to enhance your digital writing experience.",
-//     images: [{ url: "/apexo-image.svg" }],
-//     siteName: "Apexo",
-//     url: "https://www.apexo.digital/tools/oliwrite",
-//     type: "website",
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: "Apexo | OliWrite - Elevate your script writing game",
-//     description: "Oliwrite is a versatile tool offered by Apexo Digital.",
-//     images: [{ url: "/apexo-image.svg" }],
-//     site: "@apexodigital",
-//   },
-// };
+export const metadata: Metadata = {
+  title: "Apexo | OliWrite - Elevate your script writing game",
+  description:
+    "Oliwrite is a versatile tool offered by Apexo Digital, designed to enhance your digital writing experience.",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  abstract: "width=device-width, initial-scale=1",
+  appleWebApp: true,
+  applicationName: "Apexo",
+  category: "Business",
+  classification: "Business",
+  keywords: [
+    "Apexo Digital",
+    "Oliwrite",
+    "writing tool",
+    "digital writing",
+    "content creation",
+    "writing enhancement",
+  ],
+  openGraph: {
+    title: "Apexo | OliWrite - Elevate your script writing game",
+    description:
+      "Oliwrite is a versatile tool offered by Apexo Digital, designed to enhance your digital writing experience.",
+    images: [{ url: "/apexo-image.svg" }],
+    siteName: "Apexo",
+    url: "https://www.apexo.digital/tools/oliwrite",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Apexo | OliWrite - Elevate your script writing game",
+    description: "Oliwrite is a versatile tool offered by Apexo Digital.",
+    images: [{ url: "/apexo-image.svg" }],
+    site: "@apexodigital",
+  },
+};
 
 export default function Page({ params }: { params: { chatId: string[] } }) {
   return (
